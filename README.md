cb21bf585a6f1a67d15555adc02754ae8d8e9db5
Lucas Zanazzo <lucaszanazzo4@gmail.com>
2023-08-13 19:02:55 -0300

    Problemas con el guardado
    Por alguna razon desde el .py no funciona el codigo pero desde jupyter si. Por las dudas deje el archivo de jupyter para seguir probando

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Informatorio-Data-ProyectoFinal-1-2023
+
+Nivel inicial:
+
+
+1. Obtener el archivo de la fuente que escojamos archivos de fuente utilizando Google Colab y la librería requests, por último debe almacenarse en forma local en formato CSV:
+
+Librerias necesarias:
+
+from datetime import datetime
+import requests
+import pandas as pd
+from pandas import json_normalize
+import json
+
+### API de datos abiertos del clima:
+
+Descripción: Algunas APIs públicas proporcionan datos históricos o en tiempo real sobre el clima, incluidas temperaturas, precipitaciones, vientos, etc.
+API: [https://openweathermap.org/api](https://openweathermap.org/api)
+
+2. Solicitud: Debes hacer una solicitud HTTP a la API para obtener los datos en formato JSON y luego convertirlos a CSV utilizando Python.
+
+BASE_URL = "[https://api.openweathermap.org/data/2.5/weather](https://api.openweathermap.org/data/2.5/weather)[&#34;](https://api.openweathermap.org/data/2.5/onecall/timemachine?%22)
+
+
+3. Organizar los archivos en rutas siguiendo la siguiente estructura:
+   “data_analytics\openweather\tiempodiario_yyyymmdd.csv”
+   La fecha de la nomenclatura es la fecha del tiempo tomado.
+
+4. buscar estas ciudades:
+
+cityList = ["London", "New York", "Cordoba", "Taipei", "Buenos Aires", "Mexico DF", "Dublin", "Tilfis", "Bogota", "Tokio"]
+coordList = ["lat=31&lon=64", "lat=40&lon=-73", "lat=-31&lon=-64", "lat=25&lon=64", "lat=-34&lon=-58", "lat=19&lon=-99", "lat=53&lon=6", "lat=41&lon=44", "lat=4&lon=74", "lat=35&lon=139"]
+
+5. Realizar los comentarios correspondientes para una correcta comprensión del código. (#)
--- main.py
+++ main.py
@@ -4,65 +4,79 @@ import requests
 import pandas as pd
 from pandas import json_normalize
 import json
-
-#Libreria agregada
-#import os
+from config import api_key
 
 #API
+"""
+Se puede llamar a la API de 2 formas
+-Llamada con coordenadas
+https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+- Llamada con el nombre de la city
+https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} 
+
+Para llamar a la API, uso el Nombre de las ciudades porque las coordenadas no son exactas al no tener los num despues de la coma, y termina trayendo datos de otras ciudades. A modo de mostracion dejo ambas formas de hacerlo.
+Hice 2 cambios en cityList "Mexico DF" >> 'Mexico City' y "Tilfis" >> 'Tiflis'"""
+
 base_url = "https://api.openweathermap.org/data/2.5/weather?"
 
 #Ciudades a buscar 
-cityList = ["London", "New York", "Cordoba", "Taipei", "Buenos Aires", "Mexico DF", "Dublin", "Tilfis", "Bogota", "Tokio"]
+cityList = ["London", "New York", "Cordoba", "Taipei", "Buenos Aires", "Mexico City", "Dublin", "Tiflis", "Bogota", "Tokio"]
 
 coordList = ["lat=31&lon=64", "lat=40&lon=-73", "lat=-31&lon=-64", "lat=25&lon=64", "lat=-34&lon=-58", "lat=19&lon=-99", "lat=53&lon=6", "lat=41&lon=44", "lat=4&lon=74", "lat=35&lon=139"]
 
-api_key = "cfc35e951443dc18490d3fe2188e7267"#Esta key deberia ser llamada desde otro archivo
 
 #Variable donde vamos a almacenar los registros
-total_registros = []
+#total_registros_coordenadas = []
+total_registros_city = []
 
+#Funcione que obtiene los datos de la API
+def obtener_registros():
+    for city, coordenadas in zip(cityList, coordList):
+        url_completa = f"{base_url}q={city}&appid={api_key}&units=metric"
+        #Request a la API y guardamos cada requests en una lista
+        response = requests.get(url_completa).json()
+        total_registros_city.append(response)
 
-def registros(ciudad, coordenadas):
-    #url_completa = f"{base_url}q={ciudad}&appid={api_key}&units=metric"
-    url_completa = f"{base_url}{coordenadas}&appid={api_key}&units=metric" 
+        #En caso de querer usar coordenadas 
+        #url_completa2 = f"{base_url}{coordenadas}&appid={api_key}&units=metric"
+        #response2= requests.get(url_completa2).json()
+        #total_registros_coordenadas.append(response2)
 
-    #Request a la api 
-    response = requests.get(url_completa).json()
+        
+#Pasamos los registros a DATAFRAME
+def transformando_datos():
+    df = pd.json_normalize(total_registros_city) 
     
-    #Obteniendo datos de interes 
-    dt_value = response['dt']
-    descripcion = response['weather'][0]['description']
-    temperatura = response['main']['temp']
-    temperatura_min = response['main']['temp_min']
-    temperatura_max = response['main']['temp_max']
-    sensacion_termica = response['main']['feels_like']
-    humedad = response['main']['humidity']
-    velocidad_viento = response['wind']['speed']
+    df = df.drop(['weather', 'base', 'sys.type', 'sys.id', 'cod','main.temp_max', 'main.temp_min', 'id'], axis=1)
+    df.rename(columns = {'dt':'datetime','timezone':'timezone offset', 'name': 'city name','coord.lon':'longitud' ,'coord.lat' :'latitud', 'main.temp':'temperature', 'main.feels_like':'feels Like', 'main.pressure':'pressure', 'main.humidity':'humidity', 'main.sea_level':'Sea Level', 'wind.speed': 'wind speed', 'wind.deg': 'wind deg', 'wind.gust': 'wind gust', 'clouds.all': 'clouds', 'sys.country': 'country', 'sys.sunrise': 'sunrise','sys.sunset': 'sunset'}, inplace = True)
 
-    return {"City Name": city,"Date": dt_value, "Descripcion Del Tiempo": descripcion, "Temperatura": temperatura, "Temperatura Min": temperatura_min, "Temperatura Max": temperatura_max, "Sensacion Termica": sensacion_termica, "Humedad": humedad, "Velocidad Viento": velocidad_viento}
+    datecols = ['datetime', 'sunrise', 'sunset']
+    df[datecols] = df[datecols].apply(lambda x: pd.to_datetime(x, unit='s').dt.date)
+    
+    #Ejemplo del df2
+    #df2 = pd.json_normalize(total_registros_coordenadas) 
+    #df2 = df2.drop(['weather', 'base', 'sys.type', 'sys.id', 'cod','main.grnd_level','main.temp_max', 'main.temp_min', 'id'], axis=1)
+    #df2.rename(columns = {'dt':'datetime','timezone':'timezone offset', 'name': 'city name','coord.lon':'longitud' ,'coord.lat' :'latitud', 'main.temp':'temperature', 'main.feels_like':'feels Like', 'main.pressure':'pressure', 'main.humidity':'humidity', 'main.sea_level':'Sea Level', 'wind.speed': 'wind speed', 'wind.deg': 'wind deg', 'wind.gust': 'wind gust', 'clouds.all': 'clouds', 'sys.country': 'country', 'sys.sunrise': 'sunrise','sys.sunset': 'sunset'}, inplace = True) 
 
+    return df 
 
 #Control de ejecucion 
 if __name__ == '__main__':
-    for city, coordenadas in zip(cityList, coordList):
-        data = registros(city, coordenadas)
-        total_registros.append(data)
-
-    #Paso los datos obtenidos a un DATAFRAME
-    df = pd.DataFrame(total_registros)
-
+    #ejecutando funciones:
+    obtener_registros()
+    df = transformando_datos()
+    
     #Registrando la fecha de ejecucion
     fecha_actual = datetime.now().strftime("%Y%m%d")
 
-    
-    # Obtener la ruta absoluta
-    path_file = f'data_analytics/openweather/tiempodiario_{fecha_actual}.csv'
-    #absolute_path = os.path.abspath(path_file)
+    #Ruta donde almacenaremos el archivo y su fecha
+    path_file = f'data_analytics/openweather/tiempodiario_{fecha_actual}.csv' 
 
     #archivando datos
     with open(path_file, 'w') as output_file:
-        df.to_csv( output_file, index=False)
-
+        df.to_csv(output_file, encoding="UTF8", sep=",", index=False)
+        print(df)
 
 
 
